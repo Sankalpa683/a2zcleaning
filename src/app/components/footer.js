97f437471f2cacf6831f6dbbc5c41900a6f3757d
Sankalpa683 <sank.dahal@gmail.com>
2024-09-14 21:50:21 +0545

    footer section done

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from 'react-icons/fa';
 
-const footer = () => {
+const Footer = () => {
   return (
     <>
-    <section className='py-4 pb-6 h-fit z-50 absolute w-full'>
+      <section className='py-4 pb-6 h-fit z-50 absolute w-full bg-white'>
         <div className='flex flex-col px-2 lg:px-8 items-center justify-between'>
-            <div className='pb-2'>
-                <img src="/footer.png" width={210}/>
+          <div className='flex items-center justify-center text-center w-full text-gray-500 mt-4'>
+            <p>Copyright &copy; 2024 <span className='text-[#007aff]'>A2Z Cleaning Service</span>. All Rights Reserved.</p>
+          </div><br />
+          <div className='flex flex-col sm:flex-col justify-center gap-4 items-center space-y-2'>
+            
+            <div className='flex items-center space-x-2 text-gray-600'>
+              <FaEnvelope className='text-[#007aff]' />
+              <span>atozhomerepairservice@gmail.com</span>
             </div>
-            <div className='flex items-center justify-center text-center w-full break-all text-gray-500'>
-                <p>Copyright &copy; 2024 <span className='text-[#007aff]'>A2Z Cleaning Service</span>. All Rights Reserved.</p>
+            
+            <div className='flex items-center space-x-2 text-gray-600'>
+              <FaPhoneAlt className='text-[#007aff]' />
+              <span>+977 9812107370, 9762272048</span>
             </div>
+            
+            <div className='flex items-center space-x-2 text-gray-600'>
+              <FaMapMarkerAlt className='text-[#007aff]' />
+              <span>Gothatar, Kathmandu, Nepal</span>
+            </div>
+            
+          </div>
         </div>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default footer
+export default Footer;
